af11cdbb315ee4b8bdf5b7411786e8d9375794e4
Hiroaki Nitobe <h.nitobe@gmail.com>
2022-03-20 09:29:30 +0900

    mod

--- pi_agm/src/main.rs
+++ pi_agm/src/main.rs
@@ -8,18 +8,26 @@
 
 /// rug: C言語 GMP 多倍長ライブラリの rust ラッパ
 use rug::Float;
+use quanta::Clock;
 
 /// 1/log(2)=3.3219280948873623478703194294894
 /// の一億倍
 //const PREC: u32 = 332192809 + 33;
-const PREC: u32 = 332 + 33;
+const PREC: u32 = 3321928 + 33;
 const MAX:  u32 = 28;
 
 fn main() {
+    let clock = Clock::new();
+    let start = clock.now();
+
     let mut a0 = Float::with_val(PREC, 1.0);
     let mut b  = Float::with_val(PREC, 2.0).recip_sqrt();
     let mut t0 = Float::with_val(PREC, 0.25);
     let mut p  = Float::with_val(PREC, 1.0);
+
+    let mut now = clock.now();
+    eprintln!("init:  {:?}", now.duration_since(start));
+    let mut lap = now;
 
     for _i in 1..=MAX {
         let a1 = Float::with_val(PREC, &a0 + &b) / 2;
@@ -29,12 +37,24 @@ fn main() {
         t0 = Float::with_val(PREC, &t0 - &t1);
         p  = Float::with_val(PREC, &p) * 2;
         a0 = Float::with_val(PREC, &a1);
+
     }
+    now = clock.now();
+    eprintln!("loop:  {:?}", now.duration_since(lap));
+    lap = now;
+
     a0 = Float::with_val(PREC, &a0 + &b);
     a0 = Float::with_val(PREC, &a0 * &a0);
     t0 = Float::with_val(PREC, &t0) * 4;
     a0 = Float::with_val(PREC, &a0 / &t0);
 
+    now = clock.now();
+    eprintln!("end:   {:?}", now.duration_since(lap));
+    lap = now;
+
     println!("{}", a0);
+    now = clock.now();
+    eprintln!("print: {:?}", now.duration_since(lap));
+    eprintln!("total: {:?}", now.duration_since(start));
 
 }
