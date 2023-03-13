c4715825bf087e15c3f41bd14da3885e6c3e1c80
Hiroaki Nitobe <h.nitobe@gmail.com>
2023-03-14 07:12:41 +0900

    new

---
+++ pi_ln/pi_ln.py
@@ -0,0 +1,13 @@
+#https://tsujimotter.hatenablog.com/entry/pi-approximation-formula
+
+from decimal import *
+
+getcontext().prec = 35
+
+print((Decimal('640320')**Decimal('3')+Decimal('744')).ln()/Decimal('163').sqrt())
+
+## python pi_ln.py
+#3.1415926535897932384626433832797267
+## pi 35
+#3.1415926535897932384626433832795028
+#  123456789012345678901234567890
