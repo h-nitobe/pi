0e7c1ab94ced6659698f95d424a0a9f00cc525fe
Hiroaki Nitobe <h.nitobe@gmail.com>
2023-03-14 09:02:32 +0900

    mod

--- pi_ln/pi_ln.py
+++ pi_ln/pi_ln.py
@@ -4,7 +4,7 @@ from decimal import *
 
 getcontext().prec = 35
 
-print((Decimal('640320')**Decimal('3')+Decimal('744')).ln()/Decimal('163').sqrt())
+print((Decimal('640320')**3+744).ln()/Decimal('163').sqrt())
 
 ## python pi_ln.py
 #3.1415926535897932384626433832797267
